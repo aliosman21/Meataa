2205941573d368c82a20596d94568bd1d5b6fbf2
aliosman21 <amrali975@gmail.com>
2021-02-01 22:36:57 +0200

    profile needs pagination and real data testing

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import { MDBContainer } from "mdbreact";
 import App from "../views/App";
 import Nav from "../components/NavBar";
 import Profile from "../views/Profile";
--- src/views/Profile.js
+++ src/views/Profile.js
@@ -1,7 +1,50 @@
+import "../styles/profile.css";
+import pic from "../assets/img.jpg";
+
 function Profile() {
    return (
       <div className="main-content">
-         <h1>HELLO</h1>
+         <div className="center-content">
+            <div className="name-card">
+               <img src={pic} alt="Avatar" />
+               <div className="body-container">
+                  <h4>Ali Osman</h4>
+                  <p>Engineer</p>
+               </div>
+            </div>
+            <div className="achievements-card">
+               <h1 className="achievements-header">Achievements</h1>
+               <div className="body-container">
+                  <div className="container darker">
+                     <img src={pic} alt="Avatar" className="right" />
+                     <p>Hello. How are you today?</p>
+                     <span className="time-right">11:00</span>
+                  </div>
+               </div>
+               <div className="body-container">
+                  <div className="container darker">
+                     <img src={pic} alt="Avatar" className="right" />
+                     <p>Hello. How are you today?</p>
+                     <span className="time-right">11:00</span>
+                  </div>
+               </div>
+               <div className="body-container">
+                  <div className="container darker">
+                     <img src={pic} alt="Avatar" className="right" />
+                     <p>Hello. How are you today?</p>
+                     <span className="time-right">11:00</span>
+                  </div>
+               </div>
+               <div className="body-container">
+                  <div className="container darker">
+                     <img src={pic} alt="Avatar" className="right" />
+                     <p>Hello. How are you today?</p>
+                     <span className="time-right">11:00</span>
+                  </div>
+               </div>
+               <div>Buttons</div>
+            </div>
+         </div>
       </div>
    );
 }
